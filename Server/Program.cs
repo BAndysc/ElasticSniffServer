d794b2f6f48fc9d1bf59fb034b6454ae63a869af
Bartosz Korczyński <b.andysc@gmail.com>
2022-04-22 15:29:51 +0200

    UseForwardedHeaders

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,4 +1,5 @@
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.EntityFrameworkCore;
 using Server.Database;
 using Server.Services;
@@ -39,6 +40,11 @@ if (app.Environment.IsDevelopment())
 //app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
 
 app.MapControllers();
 
